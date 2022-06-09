eda258dfdb37e47e63ddee8593dab3021271321f
Dhaksha Rupa <Dhaksha Rupa@LAPTOP-U7IOE160>
2022-06-09 12:37:37 +0530

    commit

---
+++ Javagit/src/javaprogram/Gitsum.java
@@ -0,0 +1,13 @@
+package javaprogram;
+
+public class Gitsum {
+
+	public static void main(String[] args) {
+	int a=1000;
+	int b=500;
+	int c=a+b;
+	System.out.println("sum of two numbers: "+c);
+
+	}
+
+}
